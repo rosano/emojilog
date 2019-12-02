404fb234f8a64067113ffede38170d27110241a7
Rosano Coutinho <pub@xeari.com>
2019-12-02 17:25:07 -0500

    Scaffold service worker

---
+++ os-app/tech-sw/controller.js
@@ -0,0 +1,18 @@
+const kEMTServiceWorkerVersionID = process.env.HEROKU_SLUG_COMMIT || Date.now();
+
+const OLSKServiceWorker = require('../_shared/__external/OLSKServiceWorker/main.js');
+
+exports.OLSKControllerRoutes = function() {
+	return {
+		EMTServiceWorkerRoute: {
+			OLSKRoutePath: '/sw.js',
+			OLSKRouteMethod: 'get',
+			OLSKRouteFunction: function(req, res, next) {
+				return res.type('js').send(OLSKServiceWorker.OLSKServiceWorkerView({
+					VERSION_ID_TOKEN: kEMTServiceWorkerVersionID.toString(),
+					REFERRER_MATCH_TOKEN: res.locals.OLSKCanonicalFor('EMTTrackRoute').replace(/\//g, '\\/'),
+				}));
+			},
+		},
+	};
+};
