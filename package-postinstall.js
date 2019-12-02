4ba7c63a4252324381223d73877f735c504c9e98
Rosano Coutinho <pub@xeari.com>
2019-12-02 17:25:00 -0500

    Install document management packages

--- package-postinstall.js
+++ package-postinstall.js
@@ -15,6 +15,26 @@
 	));
 })();
 
+(function ROCOHotfixULIDForBrowserTesting() {
+	if (process.env.NODE_ENV === 'production') {
+		return;
+	}
+
+	let filePath = './node_modules/ulid/dist/index.esm.js';
+	require('fs').writeFileSync(filePath, require('fs')
+		.readFileSync(filePath, 'utf8')
+		.replace(
+			'console.error("secure crypto unusable, falling back to insecure Math.random()!");',
+			'// console.error("secure crypto unusable, falling back to insecure Math.random()!");')
+		.replace(
+			'var ulid = factory();',
+			'// var ulid = factory();')
+		.replace(
+			'export { replaceCharAt, incrementBase32, randomChar, encodeTime, encodeRandom, decodeTime, detectPrng, factory, monotonicFactory, ulid };',
+			'export { replaceCharAt, incrementBase32, randomChar, encodeTime, encodeRandom, decodeTime, detectPrng, factory, monotonicFactory };')
+	);
+})();
+
 (function OLSKPostinstallExternalAssets() {
 	const OLSKAssets = require('./node_modules/OLSKApp/modules/OLSKAssets/main.js');
 	const pathPackage = require('path');
@@ -22,5 +42,8 @@
 	OLSKAssets.OLSKAssetsCopyAssetsFromTo([
 		'normalize.css',
 		'OLSKLayout',
+		'OLSKServiceWorker',
+		'OLSKRemoteStorage',
+		'OLSKStorageWidget',
 	], pathPackage.join(__dirname, 'node_modules'), pathPackage.join(__dirname, 'os-app/_shared/__external'));
 })();
