297db50a00a1d0e58eb2feccd1f2dd1eb2d33d85
Rosano Coutinho <pub@xeari.com>
2019-12-12 18:20:14 -0500

    Tidy response render

--- os-app/open-track/controller.js
+++ os-app/open-track/controller.js
@@ -4,7 +4,7 @@ exports.OLSKControllerRoutes = function() {
 		OLSKRouteMethod: 'get',
 		OLSKRouteSignature: 'EMTTrackRoute',
 		OLSKRouteFunction: function (req, res, next) {
-			return res.render(require('path').join(__dirname, 'ui-view'), {});
+			return res.render(require('path').join(__dirname, 'ui-view'));
 		},
 		OLSKRouteLanguages: ['en'],
 		OLSKRouteMiddlewares: [
