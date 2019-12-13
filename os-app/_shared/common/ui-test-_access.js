adc19f898e334e70a2345bc9934cd01d89b9a47d
Rosano Coutinho <pub@xeari.com>
2019-12-13 12:22:25 -0500

    Replace OLSKTestingCanonicalFor

--- os-app/_shared/common/ui-test-_access.js
+++ os-app/_shared/common/ui-test-_access.js
@@ -3,7 +3,7 @@ import { deepEqual } from 'assert';
 describe('EMTCommon_Access', function testEMTCommon_Access () {
 
 	it('redirects EMTCommonIdentityRedirect', async function() {
-		deepEqual((await (await browser.fetch('http://loc.tests' + OLSKTestingCanonicalFor(require('./controller.js').OLSKControllerRoutes().EMTCommonIdentityRedirect.OLSKRoutePath))).text()).slice(0, 10), '<?xml vers')
+		deepEqual((await (await browser.fetch('http://loc.tests' + OLSKTestingCanonical(require('./controller.js').OLSKControllerRoutes().EMTCommonIdentityRedirect))).text()).slice(0, 10), '<?xml vers')
 	});
 
 });
