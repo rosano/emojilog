36ac95ca9e4026ddb95b1fe4de6ca22139e7acca
Rosano Coutinho <pub@xeari.com>
2020-09-27 11:38:35 -0300

    Tidy

--- os-app/open-track/submodules/EMTTrackDetail/rollup-start.js
+++ os-app/open-track/submodules/EMTTrackDetail/rollup-start.js
@@ -3,16 +3,16 @@ import RollupStart from './main.svelte';
 const EMTTrackDetail = new RollupStart({
 	target: document.body,
 	props: Object.assign({
-		EMTTrackDetailDispatchBack () {
+		EMTTrackDetailDispatchBack: (function () {
 			window.TestEMTTrackDetailDispatchBack.innerHTML = parseInt(window.TestEMTTrackDetailDispatchBack.innerHTML) + 1;
-		},
-		EMTTrackDetailDispatchDiscard (inputData) {
+		}),
+		EMTTrackDetailDispatchDiscard: (function (inputData) {
 			window.TestEMTTrackDetailDispatchDiscard.innerHTML = parseInt(window.TestEMTTrackDetailDispatchDiscard.innerHTML) + 1;
 			window.TestEMTTrackDetailDispatchDiscardData.innerHTML = JSON.stringify(inputData);
-		},
-		EMTTrackDetailDispatchUpdate () {
+		}),
+		EMTTrackDetailDispatchUpdate: (function () {
 			window.TestEMTTrackDetailDispatchUpdate.innerHTML = parseInt(window.TestEMTTrackDetailDispatchUpdate.innerHTML) + 1;
-		},
+		}),
 	}, Object.fromEntries(Array.from((new window.URLSearchParams(window.location.search)).entries()).map(function (e) {
 		if (['EMTTrackDetailItem'].includes(e[0])) {
 			e[1] = JSON.parse(e[1]);
--- os-app/open-track/submodules/EMTTrackMaster/rollup-start.js
+++ os-app/open-track/submodules/EMTTrackMaster/rollup-start.js
@@ -4,13 +4,13 @@ const EMTTrackMaster = new RollupStart({
 	target: document.body,
 	props: Object.assign({
 		EMTTrackMasterListItems: [],
-		EMTTrackMasterDispatchCreate () {
+		EMTTrackMasterDispatchCreate: (function () {
 			window.TestEMTTrackMasterDispatchCreate.innerHTML = parseInt(window.TestEMTTrackMasterDispatchCreate.innerHTML) + 1;
-		},
-		EMTTrackMasterDispatchSelect (inputData) {
+		}),
+		EMTTrackMasterDispatchSelect: (function (inputData) {
 			window.TestEMTTrackMasterDispatchSelect.innerHTML = parseInt(window.TestEMTTrackMasterDispatchSelect.innerHTML) + 1;
 			window.TestEMTTrackMasterDispatchSelectData.innerHTML = JSON.stringify(inputData);
-		},
+		}),
 	}, Object.fromEntries(Array.from((new window.URLSearchParams(window.location.search)).entries()).map(function (e) {
 		if (['EMTTrackMasterListItems', 'EMTTrackMasterListItemSelected'].includes(e[0])) {
 			e[1] = JSON.parse(e[1]);
