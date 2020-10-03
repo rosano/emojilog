21a71a80536b28da0ef945786eebc97850d43c00
Rosano Coutinho <pub@xeari.com>
2020-10-03 11:33:15 -0300

    Tidy

--- os-app/sub-browse/submodules/EMTBrowseListItem/ui-test-_misc.js
+++ os-app/sub-browse/submodules/EMTBrowseListItem/ui-test-_misc.js
@@ -2,19 +2,21 @@ const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
 
 describe('EMTBrowseListItem_Misc', function () {
 
+	const item = {
+		EMTMemoEventDate: new Date('2019-02-23T13:56:36Z'),
+		EMTMemoNotes: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+	};
+
 	before(function () {
 		return browser.OLSKVisit(kDefaultRoute, {
-			EMTBrowseListItemObject: JSON.stringify({
-				EMTMemoEventDate: new Date('2019-02-23T13:56:36Z'),
-				EMTMemoNotes: 'alfa',
-			}),
+			EMTBrowseListItemObject: JSON.stringify(item),
 		});
 	});
 
 	describe('EMTBrowseListItemEventDate', function test_EMTBrowseListItemEventDate () {
 		
 		it('binds EMTMemoEventDate', function () {
-			browser.assert.text(EMTBrowseListItemEventDate, Date.parse('2019-02-23T13:56:36Z'));
+			browser.assert.text(EMTBrowseListItemEventDate, item.EMTMemoEventDate.valueOf().toString());
 		});
 		
 		it('binds EMTMemoNotes', function () {
