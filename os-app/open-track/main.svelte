50b8a3d9098366be928e43dedea7f1d27f5f9653
Rosano Coutinho <pub@xeari.com>
2020-10-18 11:05:59 -0300

    Purge _OLSKAppToolbarDispatchImport

--- os-app/open-track/main.svelte
+++ os-app/open-track/main.svelte
@@ -401,7 +401,6 @@ import OLSKStorageWidget from 'OLSKStorageWidget';
 		OLSKAppToolbarStorageStatus={ mod._ValueFooterStorageStatus }
 		OLSKAppToolbarDispatchStorage={ mod.OLSKAppToolbarDispatchStorage }
 		_OLSKAppToolbarDispatchExport={ mod._OLSKAppToolbarDispatchExport }
-		_OLSKAppToolbarDispatchImport={ mod._OLSKAppToolbarDispatchImport }
 		OLSKAppToolbarDispatchLauncher={ mod.OLSKAppToolbarDispatchLauncher }
 		/>
 </footer>
