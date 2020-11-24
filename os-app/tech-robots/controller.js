92a8e9e6113d54741a914d9e266cb3b67d0391a3
Rosano Coutinho <pub@xeari.com>
2020-11-24 13:13:34 -0300

    Define robots.txt

---
+++ os-app/tech-robots/controller.js
@@ -0,0 +1,18 @@
+exports.OLSKControllerRoutes = function() {
+	return [{
+		OLSKRoutePath: '/robots.txt',
+		OLSKRouteMethod: 'get',
+		OLSKRouteSignature: 'EMTRobotsRoute',
+		OLSKRouteFunction (req, res, next) {
+			return res.send(require('OLSKRobots').OLSKRobotsTXT([
+				'EMTVitrineRoute',
+			].reduce(function (coll, item) {
+				return coll.concat(res.locals.OLSKCanonicalFor(item)).concat((res.locals.OLSKRouteObjectFor(item).OLSKRouteLanguages || []).map(function (e) {
+					return res.locals.OLSKCanonicalLocalizedFor(item, {
+						OLSKRoutingLanguage: e,
+					});
+				}));
+			}, [])));
+		},
+	}];
+};
