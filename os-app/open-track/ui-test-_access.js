8536c70a8ee4542a53e24f6fead8c9740d6ce023
Rosano Coutinho <pub@xeari.com>
2020-12-04 13:20:01 -0300

    Propagate OLSKLanguageSwitcher

--- os-app/open-track/ui-test-_access.js
+++ os-app/open-track/ui-test-_access.js
@@ -58,6 +58,26 @@ describe('EMTTrack_Access', function () {
 		return browser.assert.OLSKLauncherItems('EMTTrackMasterLauncherFakeItemProxy', 1);
 	});
 
+	describe('OLSKLanguageSwitcherLauncher', function test_OLSKLanguageSwitcherLauncher() {
+
+		before(function () {
+			return browser.pressButton('.OLSKAppToolbarLanguageButton');
+		});
+
+		before(function () {
+			return browser.fill('.LCHLauncherFilterInput', 'OLSKLanguageSwitcherLauncherFakeItemProxy');
+		});
+
+		it('shows OLSKLanguageSwitcherLauncherFakeItemProxy', function () {
+			browser.assert.elements('.LCHLauncherPipeItem', 1);
+		});
+
+		after(function () {
+			browser.pressButton('#TestLCHDebugCloseButton');
+		});
+
+	});
+
 	context('click OLSKAppToolbarStorageButton', function () {
 		
 		before(function () {
