f22cf47ccc8756c5101631816284871ae7910780
Rosano Coutinho <pub@xeari.com>
2021-02-09 17:08:07 -0300

    Spec sort by touch

--- os-app/open-track/ui-logic.js
+++ os-app/open-track/ui-logic.js
@@ -1,6 +1,14 @@
 const mod = {
 
 	EMLTrackSort (a, b) {
+		if (a.EMLJournalTouchDate && b.EMLJournalTouchDate) {
+			return b.EMLJournalTouchDate - a.EMLJournalTouchDate;
+		}
+
+		if (a.EMLJournalTouchDate || b.EMLJournalTouchDate) {
+			return a.EMLJournalTouchDate ? 1 : -1;
+		}
+
 		if (a.EMLJournalModificationDate && b.EMLJournalModificationDate) {
 			return b.EMLJournalModificationDate - a.EMLJournalModificationDate;
 		}
