ef787463c2b4d983ce7d1c12d5823e2f9c1be3d7
Rosano Coutinho <pub@xeari.com>
2021-03-08 11:29:23 -0300

    Install webnative

--- package-postinstall.js
+++ package-postinstall.js
@@ -22,5 +22,6 @@
 		'OLSKServiceWorker',
 		'OLSKUIAssets',
 		'ROCORootLink',
+		'webnative',
 	], require('path').join(__dirname, 'node_modules'), require('path').join(__dirname, 'os-app/_shared/__external'));
 })();
