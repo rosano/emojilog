rosano/kommit

Purge svelte root link

os-app/_shared/KOMRootLink/ui-test-_access.js
@@ -1,25 +1,23 @@
+const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
+
 Object.entries({
 	KOMRootLink: '.KOMRootLink',
 }).map(function (e) {
-	return global[e.shift()] = e.pop();
+	return global[e.shift()]  = e.pop();
 });
 
-require('./controller.js').OLSKControllerRoutes().forEach(function (kDefaultRoute) {
-
-	describe(`KOMRootLink_Access-${ kDefaultRoute.OLSKRouteSignature }`, function () {
-
-		before(function () {
-			return browser.OLSKVisit(kDefaultRoute);
-		});
-
-		it('shows KOMRootLink', function () {
-			browser.assert.elements(KOMRootLink, 1);
-		});
-
-		it('shows OLSKRootLink', function () {
-			browser.assert.elements('.OLSKRootLink', 1);
-		});
-
+describe('KOMRootLink_Access', function () {
+	
+	before(function() {
+		return browser.OLSKVisit(kDefaultRoute);
 	});
-
+	
+	it('shows KOMRootLink', function() {
+		browser.assert.elements(KOMRootLink, 1);
+	});
+	
+	it('shows OLSKRootLink', function() {
+		browser.assert.elements('.OLSKRootLink', 1);
+	})
+	
 });

os-app/_shared/KOMRootLink/ui-test-_localize.js
@@ -1,25 +1,23 @@
-require('./controller.js').OLSKControllerRoutes().forEach(function (kDefaultRoute) {
+const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
 
-	kDefaultRoute.OLSKRouteLanguageCodes.forEach(function (OLSKRoutingLanguage) {
+kDefaultRoute.OLSKRouteLanguageCodes.forEach(function (OLSKRoutingLanguage) {
 
-		const uLocalized = function (inputData) {
-			return OLSKTestingLocalized(inputData, OLSKRoutingLanguage);
-		};
+	const uLocalized = function (inputData) {
+		return OLSKTestingLocalized(inputData, OLSKRoutingLanguage);
+	};
 
-		describe(`KOMRootLink_Localize-${ kDefaultRoute.OLSKRouteSignature }-${ OLSKRoutingLanguage }`, function () {
+	describe(`KOMRootLink_Localize-${ kDefaultRoute.OLSKRouteSignature }-${ OLSKRoutingLanguage }`, function () {
 
-			before(function () {
-				return browser.OLSKVisit(kDefaultRoute, {
-					OLSKRoutingLanguage,
-				});
+		before(function() {
+			return browser.OLSKVisit(kDefaultRoute, {
+				OLSKRoutingLanguage,
 			});
+		});
 
-			it('localizes OLSKRootLink', function () {
-				browser.assert.attribute('.OLSKRootLink', 'href', OLSKTestingCanonical(require('../../open-vitrine/controller.js').OLSKControllerRoutes().shift(), {
-					OLSKRoutingLanguage,
-				}));
-			});
-
+		it('localizes OLSKRootLink', function() {
+			browser.assert.attribute('.OLSKRootLink', 'href', OLSKTestingCanonical(require('../../open-vitrine/controller.js').OLSKControllerRoutes().shift(), {
+				OLSKRoutingLanguage,
+			}));
 		});
 
 	});

os-app/_shared/KOMRootLink/ui-test-_misc.js
@@ -1,27 +1,17 @@
-require('./controller.js').OLSKControllerRoutes().forEach(function (kDefaultRoute) {
+const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
 
-	describe(`KOMRootLink_Misc--${ kDefaultRoute.OLSKRouteSignature }`, function () {
+describe('KOMRootLink_Misc', function () {
 
-		before(function () {
-			return browser.OLSKVisit(kDefaultRoute);
+	before(function () {
+		return browser.OLSKVisit(kDefaultRoute);
+	});
+
+	describe('OLSKRootLink', function () {
+		
+		it('sets OLSKRootLinkImageURL', function () {
+			browser.assert.attribute('.OLSKRootLinkImage', 'src', process.env.OLSK_LAYOUT_TOUCH_ICON_URL);
 		});
-
-		describe('OLSKRootLink', function () {
-
-			it('sets OLSKRootLinkImageURL', function () {
-				browser.assert.attribute('.OLSKRootLinkImage', 'src', '/_shared/KOMRootLink/ui-assets/identity.svg');
-			});
-
-			it('sets OLSKRootLinkImageURL', function () {
-				browser.assert.attribute('.OLSKRootLinkImage', 'src', '/_shared/KOMRootLink/ui-assets/identity.svg');
-			});
-
-			it('sets OLSKRootLinkImageURL', function () {
-				browser.assert.attribute('.OLSKRootLinkImage', 'src', '/_shared/KOMRootLink/ui-assets/identity.svg');
-			});
-
-		});
-
+	
 	});
 
 });

rosano/emojilog

os-app/_shared/EMLRootLink/ui-test-_access.js
@@ -1,25 +1,23 @@
+const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
+
 Object.entries({
 	EMLRootLink: '.EMLRootLink',
 }).map(function (e) {
 	return global[e.shift()]  = e.pop();
 });
 
-require('./controller.js').OLSKControllerRoutes().forEach(function (kDefaultRoute) {
-
-	describe(`EMLRootLink_Access-${ kDefaultRoute.OLSKRouteSignature }`, function () {
-		
-		before(function() {
-			return browser.OLSKVisit(kDefaultRoute);
-		});
-		
-		it('shows EMLRootLink', function() {
-			browser.assert.elements(EMLRootLink, 1);
-		});
-		
-		it('shows OLSKRootLink', function() {
-			browser.assert.elements('.OLSKRootLink', 1);
-		})
-		
+describe('EMLRootLink_Access', function () {
+	
+	before(function() {
+		return browser.OLSKVisit(kDefaultRoute);
 	});
 	
-})
+	it('shows EMLRootLink', function() {
+		browser.assert.elements(EMLRootLink, 1);
+	});
+	
+	it('shows OLSKRootLink', function() {
+		browser.assert.elements('.OLSKRootLink', 1);
+	})
+	
+});

os-app/_shared/EMLRootLink/ui-test-_localize.js
@@ -1,27 +1,25 @@
-require('./controller.js').OLSKControllerRoutes().forEach(function (kDefaultRoute) {
+const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
 
-	kDefaultRoute.OLSKRouteLanguageCodes.forEach(function (OLSKRoutingLanguage) {
+kDefaultRoute.OLSKRouteLanguageCodes.forEach(function (OLSKRoutingLanguage) {
 
-		const uLocalized = function (inputData) {
-			return OLSKTestingLocalized(inputData, OLSKRoutingLanguage);
-		};
+	const uLocalized = function (inputData) {
+		return OLSKTestingLocalized(inputData, OLSKRoutingLanguage);
+	};
 
-		describe(`EMLRootLink_Localize-${ kDefaultRoute.OLSKRouteSignature }-${ OLSKRoutingLanguage }`, function () {
+	describe(`EMLRootLink_Localize-${ kDefaultRoute.OLSKRouteSignature }-${ OLSKRoutingLanguage }`, function () {
 
-			before(function() {
-				return browser.OLSKVisit(kDefaultRoute, {
-					OLSKRoutingLanguage,
-				});
+		before(function() {
+			return browser.OLSKVisit(kDefaultRoute, {
+				OLSKRoutingLanguage,
 			});
+		});
 
-			it('localizes OLSKRootLink', function() {
-				browser.assert.attribute('.OLSKRootLink', 'href', OLSKTestingCanonical(require('../../open-vitrine/controller.js').OLSKControllerRoutes().shift(), {
-					OLSKRoutingLanguage,
-				}));
-			});
-
+		it('localizes OLSKRootLink', function() {
+			browser.assert.attribute('.OLSKRootLink', 'href', OLSKTestingCanonical(require('../../open-vitrine/controller.js').OLSKControllerRoutes().shift(), {
+				OLSKRoutingLanguage,
+			}));
 		});
 
 	});
 
-})
+});

os-app/_shared/EMLRootLink/ui-test-_misc.js
@@ -1,19 +1,17 @@
-require('./controller.js').OLSKControllerRoutes().forEach(function (kDefaultRoute) {
+const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
 
-	describe(`EMLRootLink_Misc--${ kDefaultRoute.OLSKRouteSignature }`, function () {
+describe('EMLRootLink_Misc', function () {
 
-		before(function () {
-			return browser.OLSKVisit(kDefaultRoute);
-		});
-
-		describe('OLSKRootLink', function () {
-			
-			it('sets OLSKRootLinkImageURL', function () {
-				browser.assert.attribute('.OLSKRootLinkImage', 'src', '/_shared/EMLRootLink/ui-assets/identity.svg');
-			});
-		
-		});
-
+	before(function () {
+		return browser.OLSKVisit(kDefaultRoute);
 	});
+
+	describe('OLSKRootLink', function () {
+		
+		it('sets OLSKRootLinkImageURL', function () {
+			browser.assert.attribute('.OLSKRootLinkImage', 'src', process.env.OLSK_LAYOUT_TOUCH_ICON_URL);
+		});
 	
+	});
+
 });