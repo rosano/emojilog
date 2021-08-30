13662803b4e14d4071cd98e4218e210be13c010b
Rosano Coutinho <pub@xeari.com>
2021-08-30 11:10:44 -0400

    Purge identity redirect

--- os-app/_shared/common/controller.js
+++ os-app/_shared/common/controller.js
@@ -1,13 +1,3 @@
-exports.OLSKControllerRoutes = function() {
-	return {
-		EMLCommonIdentityRedirect: {
-			OLSKRoutePath: '/identity.svg',
-			OLSKRouteMethod: 'get',
-			OLSKRouteRedirect: '/_shared/EMLRootLink/ui-assets/identity.svg',
-		},
-	};
-};
-
 exports.OLSKControllerSharedStaticAssetFolders = function() {
 	return [
 		'_shared/__external',
--- os-app/_shared/common/ui-test-_access.js
+++
@@ -1,7 +0,0 @@
-describe('EMLCommon_Access', function test_EMLCommon_Access () {
-
-	it('redirects EMLCommonIdentityRedirect', async function() {
-		browser.assert.deepEqual((await (await browser.fetch('http://loc.tests' + OLSKTestingCanonical(require('./controller.js').OLSKControllerRoutes().EMLCommonIdentityRedirect))).text()).slice(0, 10), '<?xml vers')
-	});
-
-});
