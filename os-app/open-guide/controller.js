6bb3715ba6abb30366c4cbadc85b5349bf25c03b
Rosano Coutinho <pub@xeari.com>
2021-10-16 21:46:29 -0400

    Hyphenate text basename

--- os-app/open-guide/controller.js
+++ os-app/open-guide/controller.js
@@ -7,7 +7,7 @@ exports.OLSKControllerRoutes = function () {
 		OLSKRouteSignature: 'EMLGuideRoute',
 		OLSKRouteFunction(req, res, next) {
 			return res.OLSKExpressLayoutRender(require('path').join(__dirname, 'ui-view'), {
-				EMLGuideContent: res.OLSKMarkdownContent(require('path').join(__dirname, `text.${ res.locals.OLSKSharedPageCurrentLanguage }.md`), Object.assign({
+				EMLGuideContent: res.OLSKMarkdownContent(require('path').join(__dirname, `text-${ res.locals.OLSKSharedPageCurrentLanguage }.md`), Object.assign({
 					EMLTrackRoute: res.locals.OLSKCanonical('EMLTrackRoute'),
 				}, Object.keys(EMLTrackTimerLogic).filter(function (e) {
 					return e.match(/EMLTrackTimerFrame[A-Z]/)
