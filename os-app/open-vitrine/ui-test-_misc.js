458404d4aaa3508f0ea8e06f60001be31ac9761d
Rosano Coutinho <pub@xeari.com>
2023-03-09 15:39:00 +0700

    Use project name for bulletin

--- os-app/open-vitrine/ui-test-_misc.js
+++ os-app/open-vitrine/ui-test-_misc.js
@@ -141,7 +141,7 @@ describe('EMLVitrine_Misc', function () {
 	describe('ROCOGazette', function test_ROCOGazette () {
 
 		it('sets ROCOBulletinProject', function () {
-			browser.assert.attribute('.ROCOBulletinProjectField', 'value', process.env.ROCO_SHARED_PROJECT_ID);
+			browser.assert.attribute('.ROCOBulletinProjectField', 'value', 'Emoji Log');
 		});
 
 	});
