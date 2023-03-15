91afa8ab3445dc8008f3ae26a1cfe0e39e2f5f42
Rosano Coutinho <pub@xeari.com>
2023-03-15 12:28:52 +0700

    Integrate OLSKAppToolbarErrorText

--- os-app/open-track/main.svelte
+++ os-app/open-track/main.svelte
@@ -441,9 +441,7 @@ const mod = {
 	},
 
 	ZDRParamDispatchWriteError (error) {
-		mod.ZDRParamDispatchError(error);
-
-		window.alert(error.message);
+		window.alert(mod._OLSKAppToolbarErrorText = error.message);
 	},
 
 	ZDRParamDispatchConnected (identity, token) {
@@ -743,6 +741,7 @@ import OLSKApropos from 'OLSKApropos';
 		OLSKAppToolbarGuideURL={ window.OLSKCanonical('EMLGuideRoute') }
 		OLSKAppToolbarFundShowProgress={ mod._ValueOLSKFundProgress }
 		OLSKAppToolbarFundLimitText={ mod._ValueDocumentRemainder }
+		OLSKAppToolbarErrorText={ mod._OLSKAppToolbarErrorText }
 		OLSKAppToolbarCloudConnected={ !!mod._ValueCloudIdentity }
 		OLSKAppToolbarCloudOffline={ mod._ValueCloudIsOffline }
 		OLSKAppToolbarCloudError={ !!mod._ValueCloudErrorText }
